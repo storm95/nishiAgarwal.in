24e8068906df35216e1f429d3c5b070488e9c653
Shashank Shekhar <shashank.95@gmail.com>
2016-10-31 21:00:25 +0530

    Comment added to connect to database

--- dbConnect.php
+++ dbConnect.php
@@ -1,6 +1,6 @@
 <?php
 	$db_not_connect_error= 'Sorry we could not connect. Please try again.';
-	
+	//connecting to database
 	if(!@mysql_connect($host, $user, $password) || !@mysql_select_db($dbName))
 	{
 		die($db_not_connect_error);
